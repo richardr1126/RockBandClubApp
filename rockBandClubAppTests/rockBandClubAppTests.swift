c56573550d788cbb90c1cc3d22b8018cf34446f1
richardr1126 <richardr1126@gmail.com>
2020-01-27 14:38:10 -0600

    Initial Commit

---
+++ rockBandClubAppTests/rockBandClubAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  rockBandClubAppTests.swift
+//  rockBandClubAppTests
+//
+//  Created by Richard Roberson on 1/27/20.
+//  Copyright © 2020 Richard Roberson. All rights reserved.
+//
+
+import XCTest
+@testable import rockBandClubApp
+
+class rockBandClubAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
